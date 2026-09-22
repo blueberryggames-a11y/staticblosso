d759a88e0b2f6cf724c61308c9d8b46c5e0b2645
blueberryggames-a11y <blueberryggames@gmail.com>
2026-09-22 16:43:16 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: "/staticblosso",
+  assetPrefix: "/staticblosso/",
   reactStrictMode: true,
   images: {
     unoptimized: true,
